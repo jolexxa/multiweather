2eedadf967bdc02afc1aca341b8025a638f2ae17
Joanna May <joannamadisonmay@gmail.com>
2021-03-16 11:52:29 -0500

    Make floating button part of default route

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,35 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _nestedNavigatorKey = GlobalKey<NavigatorState>();
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Navigator(
+        key: _nestedNavigatorKey,
+        initialRoute: '/',
+        onGenerateRoute: (route) {
+          if (route.name == '/') {
+            return MaterialPageRoute(
+              builder: (context) {
+                return _DefaultView(navigatorKey: _nestedNavigatorKey);
+              },
+            );
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
+
+class _DefaultView extends StatelessWidget {
+  _DefaultView({Key key, @required this.navigatorKey}) : super(key: key);
+
+  final GlobalKey<NavigatorState> navigatorKey;
+
   /// Trigger the city select flow
   Future<void> Function() _addCity(BuildContext context) {
     return () async {
@@ -43,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         print('Flow changed: ${citySelectFlowController.state}');
       });
       // Start the city select flow.
-      final citySelect = await _nestedNavigatorKey.currentState.push(
+      final citySelect = await navigatorKey.currentState.push(
         CitySelectFlow.route(controller: citySelectFlowController),
       );
       // After finishing the flow, check to see how the flow finished
@@ -70,47 +99,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Navigator(
-        key: _nestedNavigatorKey,
-        initialRoute: '/',
-        onGenerateRoute: (route) {
-          if (route.name == '/') {
-            return MaterialPageRoute(
-              builder: (context) {
-                return _DefaultView();
-              },
-            );
-          }
-          return null;
-        },
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addCity(context),
         tooltip: 'Add City',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class _DefaultView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Tap the + button to get started',
-              style: Theme.of(context).textTheme.headline6,
-              textAlign: TextAlign.center,
-            ),
-          ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Tap the + button to get started',
+                style: Theme.of(context).textTheme.headline6,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
